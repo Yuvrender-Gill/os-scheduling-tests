d91242ea30dbcecbb1c7eb79dee8938d6965282a
Yuvrender Gill <yuvrender.gill@mail.utoronto.ca>
2019-11-28 23:48:01 -0500

    Added code for clock algorithm

--- a3/clock.c
+++ a3/clock.c
@@ -12,14 +12,36 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int clock_hand; // Clock hand 
+
 /* Page to evict is chosen using the clock algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 
 int clock_evict() {
-	
-	return 0;
+
+	while(1){
+
+		if ((coremap[clock_hand].pte) -> frame & PG_REF) {
+
+			// Reset reference flag if initially set
+			(coremap[clock_hand].pte) -> frame &= ~PG_REF;
+			//Increment the clock hand
+			clock_hand++; 
+
+		} else {
+
+			return clock_hand;
+		}	
+
+		// Reset clock hand on complete cycle. 
+		if (clock_hand >= memsize){
+			clock_hand = 0;
+		}
+	}
+
+	return clock_hand;
 }
 
 /* This function is called on each access to a page to update any information
@@ -27,7 +49,8 @@ int clock_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void clock_ref(pgtbl_entry_t *p) {
-
+	
+	p->frame |= PG_REF;
 	return;
 }
 
@@ -35,4 +58,6 @@ void clock_ref(pgtbl_entry_t *p) {
  * algorithm. 
  */
 void clock_init() {
+	// Initially set clock hand to 0.
+	clock_hand = 0
 }
