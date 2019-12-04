8cf1ddb6d52d7add9a1ed1bf6f41ac81f8d5b0ff
Yuvrender Gill <yuvrender.gill@mail.utoronto.ca>
2019-12-04 01:39:29 -0500

    Added rand to report.sh

--- a3/report.sh
+++ a3/report.sh
@@ -9,8 +9,11 @@ mkdir sim_results
 ./sim -f ../../../files/traces/page-simpleloop.ref -m 50 -s 10304 -a clock > ./sim_results/simple_clock_50.txt
 ./sim -f ../../../files/traces/page-simpleloop.ref -m 100 -s 10304 -a clock > ./sim_results/simple_clock_100.txt
 
+./sim -f ../../../files/traces/page-simpleloop.ref -m 50 -s 10304 -a rand > ./sim_results/simple_rand_50.txt
+./sim -f ../../../files/traces/page-simpleloop.ref -m 100 -s 10304 -a rand > ./sim_results/simple_rand_100.txt
+
 ./sim -f ../../../files/traces/page-matmul.ref -m 50 -s 10304 -a lru > ./sim_results/matmul_lru_50.txt
-./sim -f ../../../files/traces/page-matmul.ref -m 100 -s 10304 -a lru > ./sim_results/matmul_lru_50.txt
+./sim -f ../../../files/traces/page-matmul.ref -m 100 -s 10304 -a lru > ./sim_results/matmul_lru_100.txt
 
 ./sim -f ../../../files/traces/page-matmul.ref -m 50 -s 10304 -a fifo > ./sim_results/matmul_fifo_50.txt
 ./sim -f ../../../files/traces/page-matmul.ref -m 100 -s 10304 -a fifo > ./sim_results/matmul_fifo_100.txt
@@ -18,11 +21,17 @@ mkdir sim_results
 ./sim -f ../../../files/traces/page-matmul.ref -m 50 -s 10304 -a clock > ./sim_results/matmul_clock_50.txt
 ./sim -f ../../../files/traces/page-matmul.ref -m 100 -s 10304 -a clock > ./sim_results/matmul_clock_100.txt
 
+./sim -f ../../../files/traces/page-matmul.ref -m 50 -s 10304 -a rand > ./sim_results/matmul_rand_50.txt
+./sim -f ../../../files/traces/page-matmul.ref -m 100 -s 10304 -a rand > ./sim_results/matmul_rand_100.txt
+
 ./sim -f ../../../files/traces/page-blocked.ref -m 50 -s 10304 -a lru > ./sim_results/blocked_lru_50.txt
-./sim -f ../../../files/traces/page-blocked.ref -m 100 -s 10304 -a lru > ./sim_results/blocked_lru_50.txt
+./sim -f ../../../files/traces/page-blocked.ref -m 100 -s 10304 -a lru > ./sim_results/blocked_lru_100.txt
 
 ./sim -f ../../../files/traces/page-blocked.ref -m 50 -s 10304 -a fifo > ./sim_results/blocked_fifo_50.txt
 ./sim -f ../../../files/traces/page-blocked.ref -m 100 -s 10304 -a fifo > ./sim_results/blocked_fifo_100.txt
 
 ./sim -f ../../../files/traces/page-blocked.ref -m 50 -s 10304 -a clock > ./sim_results/blocked_clock_50.txt
 ./sim -f ../../../files/traces/page-blocked.ref -m 100 -s 10304 -a clock > ./sim_results/blocked_clock_100.txt
+
+./sim -f ../../../files/traces/page-blocked.ref -m 50 -s 10304 -a rand > ./sim_results/blocked_rand_50.txt
+./sim -f ../../../files/traces/page-blocked.ref -m 100 -s 10304 -a rand > ./sim_results/blocked_rand_100.txt
